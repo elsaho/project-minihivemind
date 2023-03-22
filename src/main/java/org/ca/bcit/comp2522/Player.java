998ed9afa7038648ae741b1455342661b9ced91d
elsaho <elsa.ho98@gmail.com>
2023-03-22 14:20:48 -0700

    Player

---
+++ src/main/java/org/ca/bcit/comp2522/Player.java
@@ -0,0 +1,13 @@
+package org.ca.bcit.comp2522;
+
+public class Player extends Sprite {
+
+  @Override
+  public void draw() {
+//    GameWindow.pushStyle();
+//    GameWindow.fill(this.color.getRed(), this.color.getGreen(), this.color.getBlue());
+//    GameWindow.ellipse(this.position.x, this.position.y, size, size);
+//    GameWindow.popStyle();
+
+  }
+}
