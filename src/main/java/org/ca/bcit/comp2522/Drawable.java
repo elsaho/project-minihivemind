231941f47e3dd85bd3edb92425ffcbc2f055ae37
LaggySoul <trcalaquian@gmail.com>
2023-03-25 17:20:30 -0700

    Created and fixed javadoc for all interfaces

--- src/main/java/org/ca/bcit/comp2522/Drawable.java
+++ src/main/java/org/ca/bcit/comp2522/Drawable.java
@@ -1,5 +1,11 @@
 package org.ca.bcit.comp2522;
 
+/**
+ * Drawable. The interface for all objects that can be drawn on the screen.
+ *
+ * @author Elsa Ho
+ * @version 2023
+ */
 public interface Drawable {
 
   void draw();
--- src/main/java/org/ca/bcit/comp2522/Poppable.java
+++ src/main/java/org/ca/bcit/comp2522/Poppable.java
@@ -1,5 +1,12 @@
 package org.ca.bcit.comp2522;
 
+/**
+ * Poppable. The interface for all objects that can be popped.
+ * Mainly used by the Bubble class.
+ *
+ * @author Elsa Ho
+ * @version 2023
+ */
 public interface Poppable {
 
   void pop();
