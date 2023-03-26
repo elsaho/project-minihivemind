189473657bfeb8099f3be3441371bd4a982fd220
Mai Vu <vhmai3007@gmail.com>
2023-03-26 13:09:01 -0700

    added javadoc for readability

--- src/main/java/org/ca/bcit/comp2522/Bubble.java
+++ src/main/java/org/ca/bcit/comp2522/Bubble.java
@@ -1,71 +1,72 @@
 package org.ca.bcit.comp2522;
 
-import java.awt.*;
+import java.awt.Color;
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PVector;
 
-
 /**
- * Bubble. Creates the bubbles that the player can pop.
- * Extends Sprite and implements Poppable.
+ * Bubble class creates the bubbles that the player can pop.
+ * It extends Sprite and implements Poppable.
  *
- * @author Elsa Ho
- * @author Mai Vu
+ * @author Elsa Ho, Mai Vu
  * @version 2023
- *
  */
 public class Bubble extends Sprite implements Poppable {
+  // Constants
   private static final float GRAVITY = 0.1f;
-  public PVector velocity;
-  public Line line;
-  private final PImage bubbleImage;
 
+  // Properties
+  public PVector velocity;
+  private Line line;
+  private PImage bubbleImage;
   private float startY;
+  private float currentX = size;
+  private float bounceVelocity;
+
   /**
-   * Constructor for Bubble.
+   * Constructor for Bubble class.
    *
-   * @param position as a PVector
-   * @param direction as a PVector
-   * @param size as a float
-   * @param speed as a float
-   * @param color as a Color
-   * @param window as a GameWindow
-   * @param velocity as a PVector
+   * @param position - position of the bubble
+   * @param direction - direction of the bubble
+   * @param size - size of the bubble
+   * @param speed - speed of the bubble
+   * @param color - color of the bubble
+   * @param window - GameWindow instance
+   * @param velocity - velocity of the bubble
    */
   public Bubble(PVector position, PVector direction, float size,
                 float speed, Color color, GameWindow window, PVector velocity) {
-
     super(position, direction, size, speed, color, window);
     this.velocity = velocity;
-    bubbleImage = window.loadImage("../assets/bubble.png");
-    startY = position.y;
+    this.bubbleImage = window.loadImage("../assets/bubble.png");
+    this.startY = position.y;
   }
 
-
   /**
-   * Bounce method that allows bubbles to bounce off floors and walls.
+   * Bounce method allows bubbles to bounce off floors and walls.
    */
-  private float currentX = size;
-  private final float BOUNCE_VELOCITY = - 5f; // Adjust as needed
-
   public void bounce() {
+    // Calculate bounce velocity
+    this.bounceVelocity = -6f;
+    //this.bounceVelocity = -sqrt(4 * GRAVITY * (GameWindow.getY() - this.startY + size));
 
     // Check for floor collision
-    if (position.y + size/2 >= GameWindow.getY()) {
-      position.y = GameWindow.getY() - size; // Move bubble to just above the floor
-      velocity.y = BOUNCE_VELOCITY; // Reverse velocity
+    if (this.position.y + size / 2 >= GameWindow.getY()) {
+      this.position.y = GameWindow.getY() - size; // Move bubble to just above the floor
+      this.velocity.y = bounceVelocity; // Reverse velocity
     } else {
-      position.y += velocity.y; // Move bubble along y-axis
+      this.position.y += this.velocity.y; // Move bubble along y-axis
     }
 
-    if (position.x + velocity.x > GameWindow.getX() -size) {
-      position.x = GameWindow.getX() - size;
-      velocity.x = -velocity.x;
-      currentX = position.x - size;
-    } else if (position.x + velocity.x < 0) {
-      position.x = 0;
-      velocity.x = -velocity.x;
+    // Check for wall collision
+    if (this.position.x + this.velocity.x > GameWindow.getX() - size) {
+      this.position.x = GameWindow.getX() - size;
+      this.velocity.x = -this.velocity.x;
+      this.currentX = this.position.x - size;
+    } else if (this.position.x + this.velocity.x < 0) {
+      this.position.x = 0;
+      this.velocity.x = -this.velocity.x;
     } else {
       // Add a small amount to the x position
       if (currentX == size) {
@@ -85,16 +86,26 @@ public class Bubble extends Sprite implements Poppable {
     position.add(velocity);
   }
 
+  /** This method removes the top element from the stack.
+   */
   @Override
   public void pop() {
   }
 
-
+  /** This method returns a boolean value indicating if a collision has occurred.
+   *
+   * @return true if a collision has occurred, false otherwise.
+   */
   @Override
   public boolean collided() {
     return false;
   }
 
+  /** This method displays the bubble on the screen
+   *  using the PApplet instance provided as a parameter.
+   *
+   *  @param parent the PApplet instance used to display the bubble.
+   */
   @Override
   public void display(PApplet parent) {
     parent.pushStyle();
