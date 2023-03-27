d021c595bdd7c7cdd412aded4b1548c2f0c63846
LaggySoul <trcalaquian@gmail.com>
2023-03-27 16:29:43 -0700

    Created tests for Timer, though there is one method we don't use yet.

---
+++ src/test/java/TimerTest.java
@@ -0,0 +1,45 @@
+import org.ca.bcit.comp2522.Timer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TimerTest {
+
+  Timer timerTest;
+
+  @BeforeEach
+  public void setUp() {
+    timerTest = Timer.getInstance();
+  }
+
+  @Test
+  public void testGetTimer() {
+    assert(timerTest.getStart() == 0);
+  }
+
+  @Test
+  public void testSetTimer() {
+    timerTest.setStart(1000);
+    assert(timerTest.getStart() == 1000);
+  }
+
+  @Test
+  public void testSetRemaining() {
+    timerTest.setRemaining(1000);
+    assert(timerTest.getRemaining() == 1000);
+  }
+
+  @Test
+  public void testTimeToString() {
+    timerTest.setRemaining(1000);
+    assert(timerTest.timeToString().equals("01"));
+  }
+
+  @Test
+  public void testGetInstance() {
+    Timer timerTest2 = Timer.getInstance();
+    assert(timerTest2.getStart() == 0);
+  }
+
+}
