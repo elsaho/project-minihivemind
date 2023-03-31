f7d9be1012480c12e303cad53f9167202c7a2f49
Mai Vu <vhmai3007@gmail.com>
2023-03-30 21:51:34 -0700

    delete unused method and variables

--- src/main/java/org/ca/bcit/comp2522/DatabaseHelper.java
+++ src/main/java/org/ca/bcit/comp2522/DatabaseHelper.java
@@ -9,13 +9,10 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-
-import java.util.ArrayList;
 import java.util.Date;
 
 /**
  * A helper class for connecting to MongoDB.
- *
  * This class is implemented as a singleton.
  * To get the singleton instance, use DatabaseHelper.getInstance().
  * Example usage: DatabaseHelper.getInstance(uri).put(key, value);
@@ -25,12 +22,10 @@ import java.util.Date;
  */
 public class DatabaseHelper {
   private static final String DATABASE_NAME = "BubbleTrouble";
-  private static final String COLLECTION_NAME = "level1";
   private static DatabaseHelper instance = null;
 
-  private MongoClient mongoClient;
-  private MongoDatabase database;
-  private MongoCollection<Document> collection;
+  private final MongoClient mongoClient;
+  private final MongoDatabase database;
 
   /**
    * Private constructor to prevent outside instantiation.
@@ -88,29 +83,10 @@ public class DatabaseHelper {
     if (instance != null) {
       MongoCollection<Document> scoresCollection = database.getCollection("scores");
       Document highestScoreDocument = scoresCollection.find().sort(new Document("score", -1)).limit(1).first();
-      int highestScore = (int) highestScoreDocument.getInteger("score");
-      return highestScore;
+      return highestScoreDocument.getInteger("score");
     }
     return 0;
   }
-
-  public int getPlayerScore() {
-    if (instance != null) {
-      // Get the scores collection
-      MongoCollection<Document> scoresCollection = database.getCollection("scores");
-
-      // Sort the scores by timestamp in descending order
-      Document sortCriteria = new Document("timestamp", -1);
-      ArrayList<Document> sortedScores = scoresCollection.find().sort(sortCriteria).limit(1).into(new ArrayList<>());
-
-      //get the score
-      Document mostRecentScore = sortedScores.get(0);
-      int score = mostRecentScore.getInteger("score");
-      return score;
-    }
-    return 0;
-  }
-
 
   public void close() {
     mongoClient.close();
